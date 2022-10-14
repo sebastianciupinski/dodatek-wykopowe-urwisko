f59f9356dd54d6a1441c2fb6bc3f928e67fe5496
sebastianciupinski <40261029+sebastianciupinski@users.noreply.github.com>
2022-10-14 10:50:25 +0200

    Create wykopoweUrwisko.js

---
+++ wykopoweUrwisko.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name              Wykopowe Urwisko Mod
+// @author            Sebastian Ciupinski (sebastian.ciupinski+losowywykop@gmail.com)
+// @version           1.0
+// @include           https://*.wykop.pl/*
+// ==/UserScript==
+
+
+
+
+function wykopoweUrwisko()
+{
+    (document.evaluate("//*[text()='wykopowe urwisko']", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue).innerText="wykopowe kurwisko";
+}
+
+document.onload = wykopoweUrwisko();
